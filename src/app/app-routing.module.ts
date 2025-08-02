4d4cb780dc55f73d8d41fc3b66a673985bf3fb45
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2025-08-02 19:22:49 +0530

    rrr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   { 
     path: 'apply-loan', 
     component: ApplyLoanComponent,
-    // canActivate: [AuthGuard] 
+    canActivate: [AuthGuard] 
   },
   { 
     path: 'admin/dashboard', 
